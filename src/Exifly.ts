5295f39d2950706b6e9892fd98f61650b1877e47
winjeysong <winjey-song@163.com>
2024-06-28 13:09:03 +0800

    improve type

--- src/Exifly.ts
+++ src/Exifly.ts
@@ -1,5 +1,11 @@
 import { ExifTags, TiffTags, GPSTags, StringValues } from './tags';
-import { ExiflyOpts } from './typings';
+import {
+  EXIF_IFD,
+  EXIF_TAGS,
+  ExiflyOpts,
+  GPS_INFO_IFD,
+  ZEROTH_IFD,
+} from './typings';
 
 /**
  *  Based on https://github.com/exif-heic-js/exif-heic-js
@@ -35,28 +41,54 @@ export default class Exifly {
     this.opts.debug = opts?.debug ?? false;
   }
 
-  private readTags(file: DataView, tiffStart: number, dirStart: number, strings: Record<string, string>, bigEnd: boolean) {
+  private readTags(
+    file: DataView,
+    tiffStart: number,
+    dirStart: number,
+    strings: Record<number, EXIF_IFD | ZEROTH_IFD | GPS_INFO_IFD>,
+    bigEnd: boolean,
+  ) {
     let entries = file.getUint16(dirStart, !bigEnd),
-      tags: Record<string, string | number | Number | Number[] | undefined> = {},
-      entryOffset, tag,
+      // @ts-ignore
+      tags: Record<
+        EXIF_IFD | ZEROTH_IFD | GPS_INFO_IFD,
+        string | number | Number | Number[] | undefined
+      > = {},
+      entryOffset,
+      tag,
       i;
 
     for (i = 0; i < entries; i++) {
       entryOffset = dirStart + i * 12 + 2;
       tag = strings[file.getUint16(entryOffset, !bigEnd)];
-      if (!tag && this.opts.debug) console.log("Unknown tag: " + file.getUint16(entryOffset, !bigEnd));
-      tags[tag] = this.readTagValue(file, entryOffset, tiffStart, dirStart, bigEnd);
+      if (!tag && this.opts.debug)
+        console.log('Unknown tag: ' + file.getUint16(entryOffset, !bigEnd));
+      tags[tag] = this.readTagValue(
+        file,
+        entryOffset,
+        tiffStart,
+        dirStart,
+        bigEnd,
+      );
     }
     return tags;
   }
 
-  private readTagValue(file: DataView, entryOffset: number, tiffStart: number, dirStart: number, bigEnd: boolean) {
+  private readTagValue(
+    file: DataView,
+    entryOffset: number,
+    tiffStart: number,
+    dirStart: number,
+    bigEnd: boolean,
+  ) {
     let type = file.getUint16(entryOffset + 2, !bigEnd),
       numValues = file.getUint32(entryOffset + 4, !bigEnd),
       valueOffset = file.getUint32(entryOffset + 8, !bigEnd) + tiffStart,
       offset,
-      vals, val,
-      numerator, denominator;
+      vals,
+      val,
+      numerator,
+      denominator;
 
     switch (type) {
       case 1: // byte, 8-bit unsigned int
@@ -64,7 +96,7 @@ export default class Exifly {
         if (numValues == 1) {
           return file.getUint8(entryOffset + 8);
         } else {
-          offset = numValues > 4 ? valueOffset : (entryOffset + 8);
+          offset = numValues > 4 ? valueOffset : entryOffset + 8;
           vals = [];
           for (let n = 0; n < numValues; n++) {
             vals[n] = file.getUint8(offset + n);
@@ -73,14 +105,14 @@ export default class Exifly {
         }
 
       case 2: // ascii, 8-bit byte
-        offset = numValues > 4 ? valueOffset : (entryOffset + 8);
+        offset = numValues > 4 ? valueOffset : entryOffset + 8;
         return this.getStringFromDB(file, offset, numValues - 1);
 
       case 3: // short, 16 bit int
         if (numValues == 1) {
           return file.getUint16(entryOffset + 8, !bigEnd);
         } else {
-          offset = numValues > 2 ? valueOffset : (entryOffset + 8);
+          offset = numValues > 2 ? valueOffset : entryOffset + 8;
           vals = [];
           for (let n = 0; n < numValues; n++) {
             vals[n] = file.getUint16(offset + 2 * n, !bigEnd);
@@ -99,7 +131,7 @@ export default class Exifly {
           return vals;
         }
 
-      case 5:    // rational = two long values, first is numerator, second is denominator
+      case 5: // rational = two long values, first is numerator, second is denominator
         if (numValues == 1) {
           numerator = file.getUint32(valueOffset, !bigEnd);
           denominator = file.getUint32(valueOffset + 4, !bigEnd);
@@ -136,11 +168,16 @@ export default class Exifly {
 
       case 10: // signed rational, two slongs, first is numerator, second is denominator
         if (numValues == 1) {
-          return file.getInt32(valueOffset, !bigEnd) / file.getInt32(valueOffset + 4, !bigEnd);
+          return (
+            file.getInt32(valueOffset, !bigEnd) /
+            file.getInt32(valueOffset + 4, !bigEnd)
+          );
         } else {
           vals = [];
           for (let n = 0; n < numValues; n++) {
-            vals[n] = file.getInt32(valueOffset + 8 * n, !bigEnd) / file.getInt32(valueOffset + 4 + 8 * n, !bigEnd);
+            vals[n] =
+              file.getInt32(valueOffset + 8 * n, !bigEnd) /
+              file.getInt32(valueOffset + 4 + 8 * n, !bigEnd);
           }
           return vals;
         }
@@ -148,7 +185,7 @@ export default class Exifly {
   }
 
   private getStringFromDB(buffer: DataView, start: number, length: number) {
-    let outstr = "";
+    let outstr = '';
     for (let n = start; n < start + length; n++) {
       outstr += String.fromCharCode(buffer.getUint8(n));
     }
@@ -156,64 +193,75 @@ export default class Exifly {
   }
 
   private readEXIFData(file: DataView, start: number) {
-    let bigEnd,
-      tags, tag,
-      exifData, gpsData;
+    let bigEnd, tags, tag, exifData, gpsData;
 
     // test for TIFF validity and endianness
     if (file.getUint16(start) == 0x4949) {
       bigEnd = false;
-    } else if (file.getUint16(start) == 0x4D4D) {
+    } else if (file.getUint16(start) == 0x4d4d) {
       bigEnd = true;
     } else {
-      if (this.opts.debug) console.log("Not valid TIFF data! (no 0x4949 or 0x4D4D)");
+      if (this.opts.debug)
+        console.log('Not valid TIFF data! (no 0x4949 or 0x4D4D)');
       return false;
     }
 
-    if (file.getUint16(start + 2, !bigEnd) != 0x002A) {
-      if (this.opts.debug) console.log("Not valid TIFF data! (no 0x002A)");
+    if (file.getUint16(start + 2, !bigEnd) != 0x002a) {
+      if (this.opts.debug) console.log('Not valid TIFF data! (no 0x002A)');
       return false;
     }
 
     const firstIFDOffset = file.getUint32(start + 4, !bigEnd);
 
     if (firstIFDOffset < 0x00000008) {
-      if (this.opts.debug) console.log("Not valid TIFF data! (First offset less than 8)");
+      if (this.opts.debug)
+        console.log('Not valid TIFF data! (First offset less than 8)');
       return false;
     }
 
     tags = this.readTags(file, start, start + firstIFDOffset, TiffTags, bigEnd);
 
-    if (tags.ExifIFDPointer) {
-      exifData = this.readTags(file, start, start + (tags.ExifIFDPointer as number), ExifTags, bigEnd);
+    if (tags['Exif IFD Pointer']) {
+      exifData = this.readTags(
+        file,
+        start,
+        start + (tags['Exif IFD Pointer'] as number),
+        ExifTags,
+        bigEnd,
+      );
       for (tag in exifData) {
         switch (tag) {
-          case "LightSource":
-          case "Flash":
-          case "MeteringMode":
-          case "ExposureProgram":
-          case "SensingMethod":
-          case "SceneCaptureType":
-          case "SceneType":
-          case "CustomRendered":
-          case "WhiteBalance":
-          case "GainControl":
-          case "Contrast":
-          case "Saturation":
-          case "Sharpness":
-          case "SubjectDistanceRange":
-          case "FileSource":
+          case EXIF_TAGS.EXIF_IFD.LightSource:
+          case EXIF_TAGS.EXIF_IFD.Flash:
+          case EXIF_TAGS.EXIF_IFD.MeteringMode:
+          case EXIF_TAGS.EXIF_IFD.ExposureProgram:
+          case EXIF_TAGS.EXIF_IFD.SensingMethod:
+          case EXIF_TAGS.EXIF_IFD.SceneCaptureType:
+          case EXIF_TAGS.EXIF_IFD.SceneType:
+          case EXIF_TAGS.EXIF_IFD.CustomRendered:
+          case EXIF_TAGS.EXIF_IFD.WhiteBalance:
+          case EXIF_TAGS.EXIF_IFD.GainControl:
+          case EXIF_TAGS.EXIF_IFD.Contrast:
+          case EXIF_TAGS.EXIF_IFD.Saturation:
+          case EXIF_TAGS.EXIF_IFD.Sharpness:
+          case EXIF_TAGS.EXIF_IFD.SubjectDistanceRange:
+          case EXIF_TAGS.EXIF_IFD.FileSource:
             // @ts-ignore
             exifData[tag] = StringValues[tag][exifData[tag]];
             break;
 
-          case "ExifVersion":
-          case "FlashpixVersion":
+          case EXIF_TAGS.EXIF_IFD.ExifVersion:
+          case EXIF_TAGS.EXIF_IFD.FlashpixVersion:
             // @ts-ignore
-            exifData[tag] = String.fromCharCode(exifData[tag][0], exifData[tag][1], exifData[tag][2], exifData[tag][3]);
+            exifData[tag] = String.fromCharCode(
+              exifData[tag][0],
+              exifData[tag][1],
+              exifData[tag][2],
+              exifData[tag][3],
+            );
             break;
 
-          case "ComponentsConfiguration":
+          case EXIF_TAGS.EXIF_IFD.ComponentsConfiguration:
             exifData[tag] =
               // @ts-ignore
               StringValues.Components[exifData[tag][0]] +
@@ -225,25 +273,37 @@ export default class Exifly {
               StringValues.Components[exifData[tag][3]];
             break;
         }
+        // @ts-ignore
         tags[tag] = exifData[tag];
       }
     }
 
-    if (tags.GPSInfoIFDPointer) {
-      gpsData = this.readTags(file, start, start + (tags.GPSInfoIFDPointer as number), GPSTags, bigEnd);
+    if (tags['GPS Info IFD Pointer']) {
+      gpsData = this.readTags(
+        file,
+        start,
+        start + (tags['GPS Info IFD Pointer'] as number),
+        GPSTags,
+        bigEnd,
+      );
       for (tag in gpsData) {
         switch (tag) {
-          case "GPSVersionID":
+          case EXIF_TAGS.GPS_INFO_IFD.GPSVersionID:
             // @ts-ignore
-            gpsData[tag] = gpsData[tag][0] +
+            gpsData[tag] =
+              gpsData[tag][0] +
               // @ts-ignore
-              "." + gpsData[tag][1] +
+              '.' +
+              gpsData[tag][1] +
               // @ts-ignore
-              "." + gpsData[tag][2] +
+              '.' +
+              gpsData[tag][2] +
               // @ts-ignore
-              "." + gpsData[tag][3];
+              '.' +
+              gpsData[tag][3];
             break;
         }
+        // @ts-ignore
         tags[tag] = gpsData[tag];
       }
     }
@@ -261,9 +321,9 @@ export default class Exifly {
     let exifOffset = -1;
     let ilocOffset = -1;
     for (let i = ftypeSize; i < metadataSize + ftypeSize; i++) {
-      if (this.getStringFromDB(dataView, i, 4) == "Exif") {
+      if (this.getStringFromDB(dataView, i, 4) == 'Exif') {
         exifOffset = i;
-      } else if (this.getStringFromDB(dataView, i, 4) == "iloc") {
+      } else if (this.getStringFromDB(dataView, i, 4) == 'iloc') {
         ilocOffset = i;
       }
     }
@@ -297,16 +357,22 @@ export default class Exifly {
   jpeg() {
     const data = this.buf;
     const dataView = new DataView(data);
-    if ((dataView.getUint8(0) != 0xFF) || (dataView.getUint8(1) != 0xD8)) {
-      if (this.opts.debug) console.log("Not a valid JPEG");
+    if (dataView.getUint8(0) != 0xff || dataView.getUint8(1) != 0xd8) {
+      if (this.opts.debug) console.log('Not a valid JPEG');
       return false; // not a valid jpeg
     }
     let offset = 2,
       length = data.byteLength,
       marker;
     while (offset < length) {
-      if (dataView.getUint8(offset) != 0xFF) {
-        if (this.opts.debug) console.log("Not a valid marker at offset " + offset + ", found: " + dataView.getUint8(offset));
+      if (dataView.getUint8(offset) != 0xff) {
+        if (this.opts.debug)
+          console.log(
+            'Not a valid marker at offset ' +
+              offset +
+              ', found: ' +
+              dataView.getUint8(offset),
+          );
         return false; // not a valid marker, something is wrong
       }
       marker = dataView.getUint8(offset + 1);
@@ -314,7 +380,7 @@ export default class Exifly {
       // we could implement handling for other markers here,
       // but we're only looking for 0xFFE1 for EXIF data
       if (marker == 225) {
-        if (this.opts.debug) console.log("Found 0xFFE1 marker");
+        if (this.opts.debug) console.log('Found 0xFFE1 marker');
         return this.readEXIFData(dataView, offset + 4 + 6);
       } else {
         offset += 2 + dataView.getUint16(offset + 2);
